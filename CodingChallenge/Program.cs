def170781912629d24cb781dc975ff48a5fcb6cb
Robert Romaine <robert.w.romaine@gmail.com>
2020-07-23 16:26:54 +0000

    Update Program.cs

--- CodingChallenge/Program.cs
+++ CodingChallenge/Program.cs
@@ -13,7 +13,7 @@ namespace CodingChallenge
             Console.WriteLine("Enter a number");
             string input = Console.ReadLine();
 
-            while (input != "x")
+            while (input != "-1")
             {
                 //TODO: return a list of all multiples of 3 and 5 between 0 and the number entered by the user
                 //TODO: print the list in the console
