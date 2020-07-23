3201ca6c3b7d3088e708ea814b2b3edb22d41c9f
Robert Romaine <golfingrobert@gmail.com>
2020-07-22 21:22:44 -0400

    Add project files.

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string input = Console.ReadLine();
+
+            while (input != "x")
+            {
+                //TODO: return a list of all multiples of 3 and 5 between 0 and the number entered by the user
+                //TODO: print the list in the console
+                
+                Console.WriteLine("Enter a number");
+                input = Console.ReadLine();
+            }
+        }
+    }    
+}
